af9c8b6298481eec2da0d56b12862e941fd603d8
Senaru <senaru@users.noreply.github.com>
2019-10-16 23:01:07 +1100

    Integrated AWS Cognito to Main + Minor Changes
    + Fixed Gradient Background to be smoother
    + Added Return to Login when Logout
    + Added "Back to Login Page" in Register
    + Minor Changes to Js Code
    + Changed Name of App
    + Added Titles  to Pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+//Cognito
+var data = { 
+		UserPoolId : _config.cognito.userPoolId,
+        ClientId : _config.cognito.clientId
+    };
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+    var cognitoUser = userPool.getCurrentUser();
+	
+	window.onload = function(){
+    if (cognitoUser != null) {
+        cognitoUser.getSession(function(err, session) {
+            if (err) {
+                alert(err);
+                console.log(err);
+                return;
+            }
+            console.log('session validity: ' + session.isValid());
+			//Set the profile info
+			cognitoUser.getUserAttributes(function(err, result) {
+				if (err) {
+					console.log(err);
+					return;
+				}
+				console.log(result);
+				document.getElementById("email_value").innerHTML = result[2].getValue();	
+			});			
+			
+        });
+    }
+}
+	function signOut(){
+	    if (cognitoUser != null) {
+          cognitoUser.signOut();
+          window.location.href = "file:login.html";
+        }
+	}
+
 //Start Password field
 
 // toggle password visibility
